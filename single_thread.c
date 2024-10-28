a4584130a44f670efbc953de96c4a87cb3170218
Ashby Steward-Nolan <astewardnolan@smith.edu>
2024-10-28 13:46:56 -0400

    CPU and GPU single thread with timers

---
+++ single_thread.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#define SIZE 128
+
+double get_clock() {
+    struct timeval tv; int ok;
+    ok = gettimeofday(&tv, (void *) 0);
+    if (ok<0) { 
+        printf("gettimeofday error"); 
+        }
+    return (tv.tv_sec * 1.0 + tv.tv_usec * 1.0E-6);
+}
+//ADD time!!!!
+int main() {
+  // allocate memory
+  int* input = malloc(sizeof(int) * SIZE);
+  int* output = malloc(sizeof(int) * SIZE);
+
+  // initialize inputs
+  for (int i = 0; i < SIZE; i++) {
+    input[i] = 1;
+   }
+
+  double start = get_clock();
+  // do the scan
+  for (int i = 0; i < SIZE; i++) {
+   int value = 0;
+   for (int j = 0; j <= i; j++) {
+     value += input[j];
+   }
+    output[i] = value;
+  }
+
+  double end = get_clock();
+  printf("time per call: %f ns\n", (end-start) );
+
+
+  // check results
+  for (int i = 0; i < SIZE; i++) {
+    printf("%d ", output[i]);
+  }
+  printf("\n");
+
+
+  // free mem
+  free(input);
+  free(output);
+
+  return 0;
+}
